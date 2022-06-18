0ceddeba551978c2b76d2f4f0175d3cab437cfdb
Prodegy03 <chri17052@byui.edu>
2022-06-18 12:47:55 -0600

    ok

--- CSE-210-02/hilo/game/director.py
+++ CSE-210-02/hilo/game/director.py
@@ -5,12 +5,6 @@ class Director:
     """A person who directs the game. 
     
     The responsibility of a Director is to control the sequence of play.
-
-    Attributes:
-        dice (List[Die]): A list of Die instances.
-        is_playing (boolean): Whether or not the game is being played.
-        score (int): The score for one round of play.
-        total_score (int): The score for the entire game.
     """
 
     def __init__(self):
