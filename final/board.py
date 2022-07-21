55a45052eeb0977ec6cdb4cbb96e6d45325b51ab
Prodegy03 <93844496+Prodegy03@users.noreply.github.com>
2022-07-20 21:18:56 -0600

    Add files via upload

---
+++ final/board.py
@@ -0,0 +1,66 @@
+import piece
+
+class Board():
+    
+    def __init__(self):
+       
+
+        self.board = []
+
+        
+        for i in range(8):
+            self.board.append([None] * 8)
+        # White
+        self.board[7][0] = piece.Rook(True)
+        self.board[7][1] = piece.Knight(True)
+        self.board[7][2] = piece.Bishop(True)
+        self.board[7][3] = piece.Queen(True)
+        self.board[7][4] = piece.King(True)
+        self.board[7][5] = piece.Bishop(True)
+        self.board[7][6] = piece.Knight(True)
+        self.board[7][7] = piece.Rook(True)
+
+        for i in range(8):
+            self.board[6][i] = piece.Pawn(True)
+
+        # Black
+        self.board[0][0] = piece.Rook(False)
+        self.board[0][1] = piece.Knight(False)
+        self.board[0][2] = piece.Bishop(False)
+        self.board[0][3] = piece.Queen(False)
+        self.board[0][4] = piece.King(False)
+        self.board[0][5] = piece.Bishop(False)
+        self.board[0][6] = piece.Knight(False)
+        self.board[0][7] = piece.Rook(False)
+
+        for i in range(8):
+            self.board[1][i] = piece.Pawn(False)
+
+
+    def print_board(self):
+        k = 8
+      
+
+        buffer = ""
+        for i in range(35):
+            buffer += "*"
+        print(buffer)
+        for i in range(len(self.board)):
+            tmp_str = "|"
+            for j in self.board[i]:
+                if j == None or j.name == 'GP':
+                    tmp_str += "   |"
+                elif len(j.name) == 2:
+                    tmp_str += (" " + str(j) + "|")
+                else:
+                    tmp_str += (" " + str(j) + " |")
+            print((k) , tmp_str)
+            k += -1
+        x = "    A   B   C   D   E   F   G   H"
+        buffer = ""
+        for i in range(35):
+            buffer += "*"
+        print(buffer)
+        print(x)
+
+
